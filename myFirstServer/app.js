8b471311056714b73bdc7c7ad9f341b13fc7398c
Melkeraug <54627523+Melkeraug@users.noreply.github.com>
2020-08-27 14:55:58 +0200

    läxa

---
+++ myFirstServer/app.js
@@ -0,0 +1,31 @@
+console.log("Tabben i pungen")
+
+let name = "Akta mannen git git"
+
+console.log(name)
+
+name = "Melker"
+
+console.log(name)
+
+let age = 17
+
+console.log("My age is: " + age)
+
+age ++
+
+console.log("My age next year will be: " + age)
+
+let alive = true
+
+if (!alive){
+    console.log("You are alive")
+}
+else{
+    console.log("You are dead")
+}
+
+if (age == 18) {
+    console.log("ung och dum")
+}
+
---
+++ myFirstServer/uppgift.js
@@ -0,0 +1,23 @@
+let tal1 = 1
+
+let tal2 = 2
+
+console.log(tal1 + tal2)
+
+let tal3 = 1
+
+let tal4 = 2
+
+console.log(tal3 * tal4)
+
+let tal5 = 4
+
+let tal6 = 2
+
+console.log(tal5 / tal6)
+
+let tal7 = 5
+
+let tal8 = 1
+
+console.log(tal7 - tal8)
