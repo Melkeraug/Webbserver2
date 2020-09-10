c647ac35afb0a8453ab732fc9a7ccb218bfda748
Melkeraug <54627523+Melkeraug@users.noreply.github.com>
2020-09-10 14:47:21 +0200

    ja

--- hosta_hemsida/index.js
+++ hosta_hemsida/index.js
@@ -2,7 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+app.use(express.urlencoded())
+
+const { response } = require('express');
+
 const clientDir = __dirname
+
+app.post('/', function (req, res) {
+    console.log(req.body.name.)
+    res.send('POST request to the homepage')
+})
 
 app.get('/', (req, res) => res.sendFile(clientDir + "\\index.html"))
 app.get('/index.css' , (req, res) => res.sendFile(clientDir + "\\index.css"))
